207e7fa3a93032abd6801bebc74a919b2ae46e23
ajlopez <webmaster@ajlopez.com>
2013-01-26 15:38:03 -0300

    createDatabase, value, first test

--- test/database.js
+++ test/database.js
@@ -7,3 +7,31 @@ var simpleglobals = require('..'),
 assert.ok(simpleglobals.createDatabase);
 assert.equal(typeof simpleglobals.createDatabase, 'function');
 
+// Create database
+
+var db = simpleglobals.createDatabase();
+
+assert.ok(db);
+
+// First node
+
+var customer = db.node('customer');
+
+assert.ok(customer);
+
+// Name node
+
+var name = db.node('customer', 'name');
+
+assert.ok(name);
+
+// Get value
+
+assert.ok(!name.value());
+
+// Set and Get value
+
+name.value('Adam');
+
+assert.equal(name.value(), 'Adam');
+
