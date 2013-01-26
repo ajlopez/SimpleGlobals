0f37f3188dae5c4cb77c7a73292114b3bc2444e5
ajlopez <webmaster@ajlopez.com>
2013-01-26 15:58:01 -0300

    value first tests

--- test/database.js
+++ test/database.js
@@ -21,17 +21,27 @@ assert.ok(customer);
 
 // Name node
 
-var name = db.node('customer', 'name');
+var name = db.node('customer', 1234, 'name');
 
 assert.ok(name);
 
 // Get value
 
 assert.ok(!name.value());
+assert.equal(name.value(), undefined);
 
 // Set and Get value
 
 name.value('Adam');
 
 assert.equal(name.value(), 'Adam');
+
+// Get value
+
+assert.equal(db.node('customer', 1234, 'name').value(), 'Adam');
+
+// Unset value
+
+name.value(null);
+assert.equal(db.node('customer', 1234, 'name').value(), undefined);
 
